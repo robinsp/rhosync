612792cd07fc5d4e41c58f16dd917447a10c5c5b
Robin Spainhour <robin@robinspainhour.com>
2009-10-09 17:17:05 +0200

    Define a constant for default password

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,9 @@
+class Factory 
+  DEFAULT_PASSWORD = "secret"
+end
+
 Factory.define :user do |f|
   f.sequence(:login) {|n| "defaultlogin#{n}" } 
-  f.password "secret"
+  f.password Factory::DEFAULT_PASSWORD
   f.password_confirmation {|u| u.password }
 end
