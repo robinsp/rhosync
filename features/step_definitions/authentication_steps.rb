4f51a7be858b5495a3b215913fdabe18e38a0f63
Robin Spainhour <robin@robinspainhour.com>
2009-10-14 10:57:00 +0200

    Cleared out quote escapes

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,13 +1,13 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
 Given /^I'm logged in as "([^\"]*)"$/ do |login|
-  Given "the user \"#{login}\" exists"
-  When "I login to web app as \"#{login}\""
+  Given %{the user "#{login}" exists}
+  When  %{I login to web app as "#{login}"}
 end
 
 When /^I login to web app as "([^\"]*)"$/ do |login|
-  When "I go to the login page"
-  When "I fill in \"Login\" with \"#{login}\""
-  When "I fill in \"Password\" with \"#{Factory::DEFAULT_PASSWORD}\""
-  When "I press \"Log in\""
+  When %{I go to the login page}
+  When %{I fill in "Login" with "#{login}"}
+  When %{I fill in "Password" with "#{Factory::DEFAULT_PASSWORD}"}
+  When %{I press "Log in"}
 end
