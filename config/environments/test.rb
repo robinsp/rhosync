a90e57dcf0efe8c5139760d9cdc18d3e7d4cf442
Robin Spainhour <robin@robinspainhour.com>
2009-10-22 09:29:37 +0200

    Define rspec and rspec-rails version at least 1.2.9

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,6 +22,9 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 # Gems for testing purposes
+config.gem "rspec",         :lib => "spec",         :version => ">= 1.2.9"
+config.gem "rspec-rails",   :lib => "spec/rails",   :version => ">= 1.2.9"
+
 config.gem "thoughtbot-factory_girl",
   :lib => 'factory_girl',
   :source => 'http://gems.github.com'
